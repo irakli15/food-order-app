ef02efe53cf745818f840746d303f0149ae81658
irakli15 <39444780+irakli15@users.noreply.github.com>
2022-02-09 22:35:29 -0600

    added header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Layout/Header";
+import { Fragment } from "react/cjs/react.production.min";
 
 function App() {
-  return (
-    <div></div>
-  );
+	return (
+		<Fragment>
+			<Header />
+		</Fragment>
+	);
 }
 
 export default App;
