70ce6564532da2ec9806d8d52676c13c5ba84a0b
irakli15 <39444780+irakli15@users.noreply.github.com>
2022-02-10 21:21:58 -0600

    added meal items

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import Header from "./components/Layout/Header";
 import { Fragment } from "react/cjs/react.production.min";
+import Meals from "./components/Meals/Meals";
 
 function App() {
 	return (
 		<Fragment>
 			<Header />
+      <main>
+        <Meals/>
+      </main>
 		</Fragment>
 	);
 }
