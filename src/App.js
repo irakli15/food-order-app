0926c643dc2bd5e35f139fdb00013835a10b582c
irakli15 <39444780+irakli15@users.noreply.github.com>
2022-03-12 20:08:48 -0600

    added cart modal

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import "./App.css";
 import Header from "./components/Layout/Header";
-import { Fragment } from "react/cjs/react.production.min";
+import { Fragment, useState } from "react";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 
 function App() {
+	const [cartIsShown, setCartIsShown] = useState(false);
+	const showCartHandler = () => {
+		setCartIsShown(true);
+	};
+
+	const hideCartHandler = () => {
+		setCartIsShown(false);
+	};
+
 	return (
 		<Fragment>
-			<Header />
-      <main>
-        <Meals/>
-      </main>
+			{cartIsShown && <Cart onHideCart = {hideCartHandler}/>}
+			<Header onShowCart = {showCartHandler}/>
+			<main>
+				<Meals />
+			</main>
 		</Fragment>
 	);
 }
--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -3,7 +3,7 @@ import classes from "./Input.module.css";
 const Input = (props) => {
 	return (
 		<div className={classes.input}>
-			<label hmtlFor={props.input.id}> {props.label}</label>
+			<label hmtlfor={props.input.id}> {props.label}</label>
 			<input {...props.input} />
 		</div>
 	);
